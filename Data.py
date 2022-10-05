5496677ca6063e5f85b2461c37cfe139c53e255f
AkioKiyota <berkefekeskin@gmail.com>
2022-10-05 11:24:57 +0000

    14.23 05/10/22

--- Data.py
+++ Data.py
@@ -1,28 +1,71 @@
-from asyncore import read
 import requests
-
+import random
 
 class Data:
     def __init__(self):
         f = open('ACCESS_TOKEN', 'r')
-        self.ACCESS_TOKEN = f.read()
+        self.ACCESS_TOKEN = f.read().strip()
 
-        self.response = requests.get('https://api.myanimelist.net/v2/anime',
+    def search(self, txt):
+        response = requests.get('https://api.myanimelist.net/v2/anime',
                         params={  # Query parameters / query string
-                            'q': 'Spy x Family',
+                            'q': txt,
                             'limit': 5,
                             'fields': 'id,title'
                         }, headers={
                             'Authorization': f'Bearer {self.ACCESS_TOKEN}'
                         })
 
-        self.response.raise_for_status()
-        self.result = self.response.json()
-        self.response.close()
+        response.raise_for_status()
+        result = response.json()
+        response.close()
 
+        return result
 
-    
+    def info(self, txt):
+        id = self.search(txt)['data'][0]['node']['id']
+        result = self.anime_large_info(id)
+
+        return result
+
+    def suggest(self, typ):
+        result = self.anime_suggest(typ)
+
+        animes = []
+
+        for i in result['data']:
+            anime.append(i['id'])
+
+        id = random.choice(animes)
+        anime = self.anime_large_info(id)
+
+        return anime
+
+    def anime_large_info(self, id):
+        response = requests.get('https://api.myanimelist.net/v2/anime/{id}?fields=id,title,synopsis,statistics',
+                        headers={
+                            'Authorization': f'Bearer {self.ACCESS_TOKEN}'
+                        })
+        response.raise_for_status()
+        result = response.json()
+        response.close()
+
+        return result
+
+    def anime_suggest(self, typ):
+        response = requests.get('https://api.myanimelist.net/v2/anime/ranking',
+                        params={
+                            'ranking_type': typ
+                        },
+                        headers={
+                            'Authorization': f'Bearer {self.ACCESS_TOKEN}'
+                        })
+
+        response.raise_for_status()
+        result = response.json()
+        response.close()
+
+        return result
 
 dat = Data()
 
-dat.hello()
--- Main.py
+++ Main.py
@@ -17,6 +17,36 @@ async def on_ready():
     await client.change_presence(activity=discord.Game('My Anime List!'))
     print(f'{client.user} is ready for game!!')
 
+@client.command()
+async def search(ctx, txt):
+    a = x.search(txt)
+    await ctx.send(a)
 
+@client.command()
+async def info(ctx,txt):
+    txt = txt.replace('_',' ')
+    a = x.info(txt)
+    img = a['main_picture']['large']
+    embed = discord.Embed(
+        title= a['title'],
+        description= a['synopsis'],
+        color= 0x1abc9
+    )
+    embed.set_image(url=img)
 
+    await ctx.send(embed= embed)
 
+@client.commanf()
+async def suggest(ctx, typ):
+    a= x.suggest(typ)
+
+    embed = discord.Embed(
+        title= a['title'],
+        description= a['synopsis'],
+        color= 0x1abc9
+    )
+    embed.set_image(url=a['main_picture']['large'])
+
+    await ctx.send(embed= embed)
+
+client.run(TOKEN)
